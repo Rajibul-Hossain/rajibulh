6d3217be45399723a7514cdd2de28cf214bb67be
rajibul-hossain <rajibul.hossainforschool@gmail.com>
2026-04-19 12:13:10 +0530

    added time fetching and displaying it on the lcd

--- esp_msging/main.ino
+++ esp_msging/main.ino
@@ -1,2 +1,24 @@
 #include <Wifi.h>
-#include
+#include <LiquidCrystal_I2C.h>
+#include <time.h>
+
+const char* ntp="pool.ntp.org";
+const long gmt=19800;
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+void setup(){
+    lcd.init();
+    lcd.backlight();
+    lcd.print("Booting...");
+    configTime(gmt,0, ntp);
+}
+
+void loop(){
+ struct tm t;
+ if(getLocalTime($t)){
+    char b[9];
+    strftime(b, 9, "%H:%M:%S", &t);
+    lcd.setCursor(0, 0);
+    lcd.print(b);
+ }
+}
