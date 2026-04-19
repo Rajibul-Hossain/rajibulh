e58ba715a1297fdc1a9bd4e1fc8b548fd15e233a
rajibul-hossain <rajibul.hossainforschool@gmail.com>
2026-04-19 15:41:26 +0530

    Added firebase config and webserver

--- esp_msging/main.ino
+++ esp_msging/main.ino
@@ -1,15 +1,33 @@
 #include <Wifi.h>
 #include <LiquidCrystal_I2C.h>
 #include <time.h>
-
+#include <WebServer.h>
+#include <Firebase_ESP_Client.h>
+//timezone: GMT 
 const char* ntp="pool.ntp.org";
 const long gmt=19800;
+const int dst=0;
 const char* ssid="RAJESHHOSSAIN";
 const char* pass="jinaT@15";
 int mode=0;
 bool run = 0;
 unsigned long st = 0, t = 0;
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
+//firebase credentails
+#define api "api";
+#define db "db url";
+
+FirebaseData fb;
+FirebaseAuth fb_auth;
+FirebaseConfig fb_cfg;
+
+unsigned long fb_t = 0;
+String last_msg = "", task="ot yet given", song = "not playing", msg = "rajibul hossain";
+int views = 0; maxm = 6;
+bool lb = HIGH, lp = 0, scrl = 0;
+int ix = 0;
+unsigned long bt=0, sw_s=0, sw_t=0, scr_t=0;
+long pm = 1500;
 
 void setup(){
     lcd.init();
