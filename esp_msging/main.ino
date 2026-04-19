44dd91e69fe0e0c35f7ae4b91e4e1eceec6c8ce5
rajibul-hossain <rajibul.hossainforschool@gmail.com>
2026-04-19 12:37:20 +0530

    added wifi connectivity

--- esp_msging/main.ino
+++ esp_msging/main.ino
@@ -4,6 +4,8 @@
 
 const char* ntp="pool.ntp.org";
 const long gmt=19800;
+const char* ssid="RAJESHHOSSAIN";
+const char* pass="jinaT@15";
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 void setup(){
@@ -11,6 +13,12 @@ void setup(){
     lcd.backlight();
     lcd.print("Booting...");
     configTime(gmt,0, ntp);
+    WiFi.begin(ssid,pass);
+    while(WiFi.status()!=WL_CONNECTED){
+        delay(300); lcd.print(".");
+    }
+     lcd.clear();
+     lcd.print(WiFi.localIP());
 }
 
 void loop(){
