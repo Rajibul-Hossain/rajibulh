6a895d7f19a2c4dab2e2850d0d64439acdd36377
rajibul-hossain <rajibul.hossainforschool@gmail.com>
2026-05-05 10:21:38 +0530

    added search prefixes

--- custom_home/main/scripts/home.js
+++ custom_home/main/scripts/home.js
@@ -8,3 +8,26 @@ function paste(){
         alert(":( Permission denied!");
     });
 }
+srch.addEventListener("submit", function(){
+    const prefixes = {
+        "/c": "https://codepen.io/seatch/pens?q=",
+        "/y": "https://www.youtube.com/results?search_query=",
+        "/m": "https://minecraft.fandom.com/wiki/Special:Search?query=",
+        "/s": "https://open.spotify.com/search/",
+        "/w": "https://en.wikipedia.org/index.php?search="
+    };
+    const input = q.value.trim();
+    const prefix =  input.slice(0,2);
+    const query = input.slice(2);
+    let searchURL;
+    
+    if (prefixes.hasOwnProperty(prefix)) {
+        searchURL = prefixes[prefix];
+    } else {
+        searchURL = " https://www.bing.com/search?q=" + prefix;
+    }
+
+    if (query.length > 0) {
+        window.open(searchURL + query);
+    }
+});
